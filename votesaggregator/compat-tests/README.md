8b26808bc7681b8ac22445f9c90c19e1a538ead8
arnaucube <root@arnaucube.com>
2022-05-05 15:23:47 +0200

    Synthetize circuits compat-tests code

---
+++ votesaggregator/compat-tests/README.md
@@ -0,0 +1,3 @@
+# compat-tests
+
+This directory includes tests that take as inputs the zkInputs generated from Go code (from `votesaggregator` package), and tests that everything fits well with the Circom circuit of the zkMultisig.
--- votesaggregator/compat-tests/zkinputs.test.js
+++ votesaggregator/compat-tests/zkinputs.test.js
@@ -4,18 +4,11 @@ const { expect } = require("chai");
 
 // const c_tester = require("circom_tester").c;
 const wasm_tester = require("circom_tester").wasm;
+let circuitFile = "zkinputstest.circom";
+let circuitPath = path.join(__dirname, "", circuitFile);
 
-describe("TestZKInputs 3 nMaxVotes, 3 nLevels", function () {
-	this.timeout(0);
-	let circuitFile = "zkinputstest.circom";
-	let circuitPath = path.join(__dirname, "", circuitFile);
-	let circuit;
-
-	const nMaxVotes = 3;
-	const nLevels = 3;
-
-	before( async() => {
-		console.log("generating compiled files for first time");
+async function loadCircuit(nMaxVotes, nLevels) {
+		console.log("generating circuit compiled files", nMaxVotes, nLevels);
 		const circuitCode = `
 			pragma circom 2.0.0;
 			include "./node_modules/zkmultisig/circuits/src/zkmultisig.circom";
@@ -29,30 +22,43 @@ describe("TestZKInputs 3 nMaxVotes, 3 nLevels", function () {
 		console.log("tester loaded, now going to circuit.loadConstraints");
 		await circuit.loadConstraints();
 		console.log("n_constraints", circuit.constraints.length);
-	});
+	return circuit;
+}
 
+async function testCircuit(circuit, inputsFile) {
+	const filePath = path.resolve(__dirname, inputsFile);
+	const data = fs.readFileSync(filePath);
+	const inputs = JSON.parse(data);
+
+	console.log("calculateWitness");
+	const witness = await circuit.calculateWitness(inputs, true);
+	await circuit.checkConstraints(witness);
+}
+
+describe("TestZKInputs 3 nMaxVotes, 3 nLevels", function () {
+	this.timeout(0);
+	let circuit;
+
+	const nMaxVotes = 3;
+	const nLevels = 3;
+
+	before( async() => {
+		circuit = await loadCircuit(nMaxVotes, nLevels);
+	});
 	after( async() => {
-		fs.unlinkSync(circuitPath); // comment in case of going to reuse compiled circuit
+		fs.unlinkSync(circuitPath);
 	});
 
-	it("zkinputs_3_3_1_60.json", async () => {
-		const filePath = path.resolve(__dirname, './zkinputs_3_3_1_60.json');
-		const data = fs.readFileSync(filePath);
-		const inputs = JSON.parse(data);
-		// console.log("inputs\n", inputs);
+	const tests = [
+		"zkinputs_3_3_1_60.json",
+		"zkinputs_3_3_3_60.json",
+	];
 
-		console.log("calculateWitness");
-		const witness = await circuit.calculateWitness(inputs, true);
-		await circuit.checkConstraints(witness);
-	});
-	it("zkinputs_3_3_3_60.json", async () => {
-		const filePath = path.resolve(__dirname, './zkinputs_3_3_3_60.json');
-		const data = fs.readFileSync(filePath);
-		const inputs = JSON.parse(data);
-
-		const witness = await circuit.calculateWitness(inputs, true);
-		await circuit.checkConstraints(witness);
-	});
+	for (let i=0; i<tests.length; i++) {
+		it(tests[i], async () => {
+			await testCircuit(circuit, tests[i]);
+		});
+	}
 });
 
 describe("TestZKInputs 8 nMaxVotes, 4 nLevels", function () {
@@ -65,60 +71,22 @@ describe("TestZKInputs 8 nMaxVotes, 4 nLevels", function () {
 	const nLevels = 4;
 
 	before( async() => {
-		console.log("generating compiled files for first time");
-		const circuitCode = `
-			pragma circom 2.0.0;
-			include "./node_modules/zkmultisig/circuits/src/zkmultisig.circom";
-			component main {public [chainID, processID, censusRoot, receiptsRoot, nVotes, result, withReceipts]} = zkmultisig(${nMaxVotes}, ${nLevels});
-		`;
-		await fs.writeFileSync(circuitPath, circuitCode, "utf8");
-
-		console.log("going to load tester");
-		// circuit = await c_tester(circuitPath);
-		circuit = await wasm_tester(circuitPath);
-
-		console.log("tester loaded, now going to circuit.loadConstraints");
-		await circuit.loadConstraints();
-		console.log("n_constraints", circuit.constraints.length);
+		circuit = await loadCircuit(nMaxVotes, nLevels);
 	});
-
 	after( async() => {
-		fs.unlinkSync(circuitPath); // comment in case of going to reuse compiled circuit
+		fs.unlinkSync(circuitPath);
 	});
 
-	it("zkinputs_8_4_3_60.json", async () => {
-		const filePath = path.resolve(__dirname, 'zkinputs_8_4_3_60.json');
-		const data = fs.readFileSync(filePath);
-		const inputs = JSON.parse(data);
+	const tests = [
+		"zkinputs_8_4_3_60.json",
+		"zkinputs_8_4_5_60.json",
+		"zkinputs_8_4_7_60.json",
+		"zkinputs_8_4_8_60.json",
+	];
 
-		// console.log("inputs\n", inputs);
-
-		console.log("calculateWitness");
-		const witness = await circuit.calculateWitness(inputs, true);
-		await circuit.checkConstraints(witness);
-	});
-	it("zkinputs_8_4_5_60.json", async () => {
-		const filePath = path.resolve(__dirname, 'zkinputs_8_4_5_60.json');
-		const data = fs.readFileSync(filePath);
-		const inputs = JSON.parse(data);
-
-		const witness = await circuit.calculateWitness(inputs, true);
-		await circuit.checkConstraints(witness);
-	});
-	it("zkinputs_8_4_7_60.json", async () => {
-		const filePath = path.resolve(__dirname, 'zkinputs_8_4_7_60.json');
-		const data = fs.readFileSync(filePath);
-		const inputs = JSON.parse(data);
-
-		const witness = await circuit.calculateWitness(inputs, true);
-		await circuit.checkConstraints(witness);
-	});
-	it("zkinputs_8_4_8_60.json", async () => {
-		const filePath = path.resolve(__dirname, 'zkinputs_8_4_8_60.json');
-		const data = fs.readFileSync(filePath);
-		const inputs = JSON.parse(data);
-
-		const witness = await circuit.calculateWitness(inputs, true);
-		await circuit.checkConstraints(witness);
-	});
+	for (let i=0; i<tests.length; i++) {
+		it(tests[i], async () => {
+			await testCircuit(circuit, tests[i]);
+		});
+	}
 });
