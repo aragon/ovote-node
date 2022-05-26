541a586174fe11d7fccc89b5286cd4418eccb04e
arnaucube <root@arnaucube.com>
2022-05-26 12:43:37 +0200

    Add initial prover-client package

---
+++ prover/client.go
@@ -0,0 +1,78 @@
+// Package prover implements the prover client to interact with the
+// prover-server
+package prover
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+
+	"github.com/aragon/zkmultisig-node/types"
+)
+
+// Proof represents a Groth16 zkSNARK proof
+type Proof struct {
+	// TODO maybe move this to types package
+	A        [3]*big.Int    `json:"pi_a"`
+	B        [3][2]*big.Int `json:"pi_b"`
+	C        [3]*big.Int    `json:"pi_c"`
+	Protocol string         `json:"protocol"`
+}
+
+// Client implements the prover http client, used to make requests to the
+// prover server
+type Client struct {
+	url string
+	c   *http.Client
+}
+
+// New returns a new Client for the given proverURL
+func New(proverURL string) Client {
+	httpClient := &http.Client{}
+	return Client{
+		url: proverURL,
+		c:   httpClient,
+	}
+}
+
+type errorMsg struct {
+	Message string `json:"message"`
+}
+
+// GenProof sends the given ZKInputs to the prover-server to trigger the
+// zkProof generation
+func (c *Client) GenProof(zki *types.ZKInputs) (uint64, error) {
+	jsonZKI, err := json.Marshal(zki)
+	if err != nil {
+		return 0, err
+	}
+	resp, err := c.c.Post(c.url+"/proof", "application/json", bytes.NewBuffer(jsonZKI))
+	if err != nil {
+		return 0, err
+	}
+
+	// resp.body.id contains the id to use to retrieve the proof later
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	if resp.StatusCode == http.StatusBadRequest {
+		var errMsg errorMsg
+		if err = json.Unmarshal(body, &errMsg); err != nil {
+			return 0, err
+		}
+		return 0, errors.New(errMsg.Message)
+	}
+
+	var m map[string]uint64
+	err = json.Unmarshal(body, &m)
+	if err != nil {
+		return 0, err
+	}
+
+	return m["id"], nil
+}
---
+++ prover/client_test.go
@@ -0,0 +1,50 @@
+package prover
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/aragon/zkmultisig-node/types"
+	qt "github.com/frankban/quicktest"
+	"github.com/gin-gonic/gin"
+)
+
+func TestGenProof(t *testing.T) {
+	c := qt.New(t)
+
+	r := gin.Default()
+	r.POST("/proof", mockGenProof)
+
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	p := New(ts.URL)
+	zki := types.NewZKInputs(2, 2)
+	pID, err := p.GenProof(zki)
+	c.Assert(err, qt.IsNil)
+	c.Assert(pID, qt.Equals, uint64(42))
+
+	// now with handler that returns error
+	r = gin.Default()
+	r.POST("/proof", mockGenProofErr)
+	ts = httptest.NewServer(r)
+	defer ts.Close()
+
+	p = New(ts.URL)
+	_, err = p.GenProof(zki)
+	c.Assert(err, qt.Not(qt.IsNil))
+	c.Assert(err.Error(), qt.Equals, "expected error msg")
+}
+
+func mockGenProof(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"id": 42,
+	})
+}
+
+func mockGenProofErr(c *gin.Context) {
+	c.JSON(http.StatusBadRequest, errorMsg{
+		Message: "expected error msg",
+	})
+}
